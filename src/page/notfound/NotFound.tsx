1ae8c1bee2b1bb1d63c639d574acda26c18b1686
gascol <giacatluong20121996@gmail.com>
2022-03-24 08:40:15 +0700

    [EDIT][404 page]

--- src/page/notfound/NotFound.tsx
+++ src/page/notfound/NotFound.tsx
@@ -1,5 +1,30 @@
+import styled from "styled-components"
+
 export default () => {
+  const containerStyle = {
+    margin: '50px auto 40px auto',
+    width: '600px',
+    textAlign: 'center' as const
+  }
   return (
-    <div>404 NOT FOUND</div>
+    <Container style={ containerStyle }>
+      <H1>404</H1>
+      <p><strong>Page not found</strong></p>
+    </Container>
   )
 }
+
+const Container = styled.div`
+margin: 50px auto 40px auto;
+width: 600px;
+text-align: center;
+`;
+
+const H1 = styled.h1`
+letter-spacing: -1px;
+line-height: 60px;
+font-size: 60px;
+font-weight: 100;
+margin: 0px 0 50px 0;
+text-shadow: 0 1px 0 #fff;
+`;
